0672a45282551bb89020396ad2e8c1861fa27476
daniellefrnndz <61892737+daniellefrnndz@users.noreply.github.com>
2020-07-23 10:29:27 -0700

    add map2.py file

---
+++ map2.py
@@ -0,0 +1,30 @@
+import folium
+import pandas
+
+data=pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el), fill_color=color_producer(el), color='gray', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=open(file='world.json', mode='r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fg)
+
+map.save("Map2.html")
